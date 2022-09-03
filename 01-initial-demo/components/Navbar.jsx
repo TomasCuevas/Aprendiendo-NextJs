0d09398a05478e22567ea181c166ae0ebb517e8c
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-03 16:53:41 -0300

    Barra de navegación personalizada

--- 01-initial-demo/components/Navbar.jsx
+++ 01-initial-demo/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 
+import Styles from "./Navbar.module.css";
+
 export const Navbar = () => {
   return (
-    <nav>
+    <nav className={Styles["menu-container"]}>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
