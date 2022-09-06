d4e2b55cb2244291e01db8db4c3c66a4b94528c1
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-06 09:49:09 -0300

    Next - GetStaticProps

--- 02-pokemon-static/pages/index.tsx
+++ 02-pokemon-static/pages/index.tsx
@@ -1,15 +1,26 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
 import { Button } from "@nextui-org/react";
 
 //* layout *//
 import { Layout } from "../components/layouts";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log({ props });
+
   return (
     <Layout title="Listado de Pokemons">
       <Button color={"gradient"}>Hola Mundo</Button>
     </Layout>
   );
+};
+export const getStaticProps: GetStaticProps = async () => {
+  console.log("hola mundo");
+
+  return {
+    props: {
+      name: "Tomas",
+    },
+  };
 };
 
 export default HomePage;
