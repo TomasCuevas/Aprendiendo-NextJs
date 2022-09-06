a3bafb1c6065f51680fbd9a39c151a117484d9d9
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-06 11:05:46 -0300

    Componente para las tarjetas

---
+++ 02-pokemon-static/components/pokemon/PokemonCard.tsx
@@ -0,0 +1,22 @@
+import { Card, Grid, Row, Text } from "@nextui-org/react";
+
+//* interfaces *//
+import { Pokemon } from "../../interfaces";
+
+export const PokemonCard = ({ id, img, name, url }: Pokemon) => {
+  return (
+    <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+      <Card isHoverable isPressable>
+        <Card.Body css={{ p: 1 }}>
+          <Card.Image height={150} width="100%" src={img} alt={name} />
+        </Card.Body>
+        <Card.Footer>
+          <Row justify="space-between">
+            <Text transform="capitalize">{name}</Text>
+            <Text>#{id}</Text>
+          </Row>
+        </Card.Footer>
+      </Card>
+    </Grid>
+  );
+};
--- 02-pokemon-static/pages/index.tsx
+++ 02-pokemon-static/pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage, GetStaticProps } from "next";
-import { Card, Grid, Row, Text } from "@nextui-org/react";
+import { Grid } from "@nextui-org/react";
 
 //* api *//
 import { pokeApi } from "../api";
 
 //* layout *//
 import { Layout } from "../components/layouts";
+
+//* components *//
+import { PokemonCard } from "../components/pokemon";
 
 //* interfaces *//
 import { Pokemon, PokemonListResponse } from "../interfaces";
@@ -18,20 +21,8 @@ const HomePage: NextPage<HomePageProps> = ({ pokemons }) => {
   return (
     <Layout title="Listado de Pokemons">
       <Grid.Container gap={2} justify="flex-start">
-        {pokemons.map(({ id, img, name, url }) => (
-          <Grid xs={6} sm={3} md={2} xl={1} key={id}>
-            <Card isHoverable isPressable>
-              <Card.Body css={{ p: 1 }}>
-                <Card.Image height={150} width="100%" src={img} alt={name} />
-              </Card.Body>
-              <Card.Footer>
-                <Row justify="space-between">
-                  <Text transform="capitalize">{name}</Text>
-                  <Text>#{id}</Text>
-                </Row>
-              </Card.Footer>
-            </Card>
-          </Grid>
+        {pokemons.map((pokemon) => (
+          <PokemonCard key={pokemon.id} {...pokemon} />
         ))}
       </Grid.Container>
     </Layout>
