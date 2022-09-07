fb604c97c96076c53abd499b1a7e192e4a679d7e
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-07 09:36:41 -0300

    Next - getStaticPaths

--- 02-pokemon-static/pages/pokemon/[id].tsx
+++ 02-pokemon-static/pages/pokemon/[id].tsx
@@ -1,17 +1,43 @@
 //* layout *//
+import { GetStaticProps, GetStaticPaths, NextPage } from "next";
 import { useRouter } from "next/router";
 import { Layout } from "../../components/layouts";
 
-export const PokemonPage = () => {
+interface PokemonPageProps {
+  id: string;
+  name: string;
+}
+
+export const PokemonPage: NextPage<PokemonPageProps> = ({ id, name }) => {
   const router = useRouter();
 
   console.log(router.query);
 
   return (
     <Layout>
-      <div>Hola Mundo</div>
+      <div>{`${id} - ${name}`}</div>
     </Layout>
   );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: { id: "1", name: "Bulbasaur" },
+      },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      id: 1,
+      name: "Bulbasaur",
+    },
+  };
 };
 
 export default PokemonPage;
