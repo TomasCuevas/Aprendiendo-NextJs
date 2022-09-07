34d2f27509f36c7ebbb1538bd5b202e52365e03c
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-07 10:12:25 -0300

    Generando las 150 páginas de Pokémons

--- 02-pokemon-static/pages/pokemon/[id].tsx
+++ 02-pokemon-static/pages/pokemon/[id].tsx
@@ -1,41 +1,42 @@
-//* layout *//
 import { GetStaticProps, GetStaticPaths, NextPage } from "next";
-import { useRouter } from "next/router";
+import { pokeApi } from "../../api";
+
+//* layout *//
 import { Layout } from "../../components/layouts";
 
+//* interfaces *//
+import { PokemonFull } from "../../interfaces";
+
 interface PokemonPageProps {
-  id: string;
-  name: string;
+  pokemon: PokemonFull;
 }
 
-export const PokemonPage: NextPage<PokemonPageProps> = ({ id, name }) => {
-  const router = useRouter();
-
-  console.log(router.query);
-
+export const PokemonPage: NextPage<PokemonPageProps> = ({ pokemon }) => {
   return (
-    <Layout>
-      <div>{`${id} - ${name}`}</div>
+    <Layout title={`${pokemon.name}`}>
+      <div>{pokemon.name}</div>
     </Layout>
   );
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const allPokemonms = [...Array(150)].map((value, index) => `${index + 1}`);
+
   return {
-    paths: [
-      {
-        params: { id: "1", name: "Bulbasaur" },
-      },
-    ],
+    paths: allPokemonms.map((id) => ({
+      params: { id },
+    })),
     fallback: false,
   };
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { id } = params as { id: string };
+  const { data } = await pokeApi.get<PokemonFull>(`/pokemon/${id}`);
+
   return {
     props: {
-      id: 1,
-      name: "Bulbasaur",
+      pokemon: data,
     },
   };
 };
