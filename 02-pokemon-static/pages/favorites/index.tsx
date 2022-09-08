563563ac0d59d45ae68b4a4bb9e7bedbb6207551
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-08 08:19:21 -0300

    Construir el listado de Pokémons favoritos

--- 02-pokemon-static/pages/favorites/index.tsx
+++ 02-pokemon-static/pages/favorites/index.tsx
@@ -9,6 +9,7 @@ import { Layout } from "../../components/layouts";
 
 //* components *//
 import { NoFavorites } from "../../components/ui";
+import { Card, Grid } from "@nextui-org/react";
 
 const FavoritesPage: NextPage = () => {
   const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
@@ -19,7 +20,22 @@ const FavoritesPage: NextPage = () => {
 
   return (
     <Layout title="Pokemons - Favoritos">
-      <NoFavorites />
+      {favoritePokemons.length === 0 ? (
+        <NoFavorites />
+      ) : (
+        <Grid.Container className="gap-1 flex-row justify-start">
+          {favoritePokemons.map((id) => (
+            <Grid key={id} xs={6} sm={3} md={2} xl={2}>
+              <Card isHoverable isPressable className="p-[10px]">
+                <Card.Image
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+                  className="w-full h-36"
+                />
+              </Card>
+            </Grid>
+          ))}
+        </Grid.Container>
+      )}
     </Layout>
   );
 };
