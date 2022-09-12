39408048d7d82c5e66e0db9dc0bf8ddf4e25cd3c
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-12 11:28:01 -0300

    Context Provider

--- 03-open-jira/context/ui/UIContext.tsx
+++ 03-open-jira/context/ui/UIContext.tsx
@@ -1,7 +1,31 @@
-import { createContext } from "react";
+import { useState, useContext, createContext } from "react";
 
 interface ContextProps {
   sideMenuOpen: boolean;
 }
 
 export const UIContext = createContext({} as ContextProps);
+
+export interface UIState {
+  sideMenuOpen: boolean;
+}
+
+const UI_INITIAL_STATE: UIState = {
+  sideMenuOpen: false,
+};
+
+export const UIProvider = ({ children }: { children: React.ReactNode }) => {
+  const [sideMenuOpen, setSideMenuOpen] = useState<boolean>(
+    UI_INITIAL_STATE.sideMenuOpen
+  );
+
+  return (
+    <UIContext.Provider
+      value={{
+        sideMenuOpen,
+      }}
+    >
+      {children}
+    </UIContext.Provider>
+  );
+};
