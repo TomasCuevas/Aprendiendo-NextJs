70c2421943bca497b795ddb5b811ad0894a11ee5
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-15 10:24:57 -0300

    Conectarnos a MongoDB con Mongoose

--- 03-open-jira/pages/api/seed.ts
+++ 03-open-jira/pages/api/seed.ts
@@ -1,14 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  ok: boolean;
-  msg: string;
+  name: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ ok: true, msg: "Todo correcto" });
+  res.status(200).json({ name: "Example" });
 }
