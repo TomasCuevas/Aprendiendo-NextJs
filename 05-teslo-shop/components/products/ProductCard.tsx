a92a8d86dd9581407125393cd183795c7fc00813
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-19 10:38:36 -0300

    Terminar el Product Card

--- 05-teslo-shop/components/products/ProductCard.tsx
+++ 05-teslo-shop/components/products/ProductCard.tsx
@@ -1,4 +1,12 @@
-import { Grid, Card, CardActionArea, CardMedia } from "@mui/material";
+import { useState, useMemo } from "react";
+import {
+  Typography,
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Box,
+} from "@mui/material";
 
 //* interfaces *//
 import { IProduct } from "../../interfaces";
@@ -8,17 +16,31 @@ interface ProductCardProps {
 }
 
 export const ProductCard = ({ product }: ProductCardProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <Grid item xs={6} sm={4} key={product.slug}>
+    <Grid item xs={6} sm={4}>
       <Card>
         <CardActionArea>
           <CardMedia
             component="img"
-            image={`products/${product.images[0]}`}
+            image={
+              isHovered
+                ? `products/${product.images[1]}`
+                : `products/${product.images[0]}`
+            }
             alt={product.title}
+            className="fadeIn"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
           />
         </CardActionArea>
       </Card>
+
+      <Box sx={{ mt: 1 }} className="fadeIn">
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={300}>{`$${product.price}`}</Typography>
+      </Box>
     </Grid>
   );
 };
