397837089ba52aae4517f9f8bc82f73e2397263b
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-20 16:52:01 -0300

    AuthLayout

---
+++ 05-teslo-shop/components/layouts/AuthLayout.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import { Box } from "@mui/material";
+
+//* interface *//
+interface AuthLayoutProps {
+  children: React.ReactNode;
+  title: string;
+}
+
+export const AuthLayout = ({ children, title }: AuthLayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title></title>
+      </Head>
+
+      <main>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="cawlc(100vh - 200px)"
+        >
+          {children}
+        </Box>
+      </main>
+    </>
+  );
+};
