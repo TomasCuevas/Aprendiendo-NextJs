b52317551951603d5f13925840e063c91fcc3969
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-26 15:54:03 -0300

    Mantener el query parameter después de crear un usuario

--- 05-teslo-shop/pages/auth/login.tsx
+++ 05-teslo-shop/pages/auth/login.tsx
@@ -113,7 +113,14 @@ const LoginPage = () => {
             </Grid>
 
             <Grid item xs={12} display="flex" justifyContent="end">
-              <NextLink href="/auth/register" passHref>
+              <NextLink
+                href={
+                  router.query?.p
+                    ? `/auth/register?p=${router.query.p}`
+                    : "/auth/register"
+                }
+                passHref
+              >
                 <Link>No tienes cuenta?</Link>
               </NextLink>
             </Grid>
