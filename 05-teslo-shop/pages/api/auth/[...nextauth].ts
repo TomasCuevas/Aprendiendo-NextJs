7f8340906772ef32c9d2b163cbbe359c17bc01f8
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-27 10:14:02 -0300

    Proveedor - Credenciales

--- 05-teslo-shop/pages/api/auth/[...nextauth].ts
+++ 05-teslo-shop/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,34 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+    Credentials({
+      name: "Custom Login",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "correo@google.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "password",
+        },
+      },
+      async authorize(credentials) {
+        console.log(credentials);
+
+        return null;
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    // ...add more providers here
   ],
 };
 
