437df2bf538d4eb632f4931a6b5103093c175a94
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-28 11:37:30 -0300

    Crear orden - Backend

--- 05-teslo-shop/pages/api/orders/index.ts
+++ 05-teslo-shop/pages/api/orders/index.ts
@@ -1,4 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+
+import { IOrder } from "../../../interfaces/order";
+
+//* database *//
+import { connect } from "../../../database/config";
+import Product from "../../../database/models/Product";
 
 type Data = {
   message: string;
@@ -16,8 +23,33 @@ export default function handler(
       return res.status(400).json({ message: "Bad request!" });
   }
 }
-const createOrder = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const body = req.body;
+const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { orderItems, total } = req.body as IOrder;
 
-  return res.status(200).json(body);
+  const session = await getSession({ req });
+  if (!session) {
+    return res.status(400).json({
+      message: "Debe de estar autenticado.",
+    });
+  }
+
+  const productsIds = orderItems.map((product) => product._id);
+  await connect();
+
+  const dbProducts = await Product.find({ _id: { $in: productsIds } });
+
+  try {
+    const subtotal = orderItems.reduce((prevState, current) => {
+      const currentPrice = dbProducts.find(
+        (product) => product._id === current._id
+      )?.price;
+      if (!currentPrice) {
+        throw new Error("Verifique el carrito. Producto no existe.");
+      }
+
+      return currentPrice * current.quantity + prevState;
+    }, 0);
+  } catch (error) {}
+
+  return res.status(200).json({ message: "example" });
 };
