558bdb642345679e1fbb6b5ef51d7e3e170b5217
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-29 16:46:30 -0300

    Paypal OAuth Token

--- 05-teslo-shop/pages/api/orders/pay.ts
+++ 05-teslo-shop/pages/api/orders/pay.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 
 type Data = {
   message: string;
@@ -17,6 +18,48 @@ export default function handler(
   }
 }
 
+const getPaypalBearerToken = async (): Promise<string | null> => {
+  const PAYPAL_CLIEND = process.env.NEXT_PUBLIC_PAYPAL_CLIEND;
+  const PAYPAL_SECRET = process.env.PAYPAL_SECRET;
+
+  const base64Token = Buffer.from(
+    `${PAYPAL_CLIEND}:${PAYPAL_SECRET}`,
+    "utf-8"
+  ).toString("base64");
+  const body = new URLSearchParams("grant_type=client_credentials");
+
+  try {
+    const { data } = await axios.post(
+      process.env.PAYPAL_OAUTH_URL || "",
+      body,
+      {
+        headers: {
+          Authorization: `Basic ${base64Token}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    return data.access_token;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log(error.response?.data);
+    } else {
+      console.log(error);
+    }
+
+    return null;
+  }
+};
+
 const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  return res.status(200).json({ message: "Orden pagada :)" });
+  const paypalBearerToken = await getPaypalBearerToken();
+
+  if (!paypalBearerToken) {
+    return res
+      .status(400)
+      .json({ message: "No se pudo confirmar el token de paypal." });
+  }
+
+  return res.status(200).json({ message: `${paypalBearerToken}` });
 };
