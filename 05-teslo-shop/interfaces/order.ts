fef8d5b7e91976913010d9772bdea026c1469efa
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-30 17:01:06 -0300

    Mostrar todas las ordenes

--- 05-teslo-shop/interfaces/order.ts
+++ 05-teslo-shop/interfaces/order.ts
@@ -14,6 +14,7 @@ export interface IOrder {
   total: number;
   transactionId?: String;
   user?: IUser | string;
+  createdAt: string;
 }
 
 export interface IOrderItem {
--- 05-teslo-shop/pages/admin/orders.tsx
+++ 05-teslo-shop/pages/admin/orders.tsx
@@ -1,4 +1,6 @@
 import { NextPage, GetServerSideProps } from "next";
+import { Chip, Grid } from "@mui/material";
+import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 
 //* icons *//
 import { ConfirmationNumberOutlined } from "@mui/icons-material";
@@ -8,14 +10,86 @@ import { AdminLayout } from "../../components/layouts/AdminLayout";
 
 //* utils *//
 import { verifyAdminInPage } from "../../utils/verifyAdminInPage";
+import useSWR from "swr";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "Order ID", width: 250 },
+  { field: "email", headerName: "Email", width: 200 },
+  { field: "name", headerName: "Nombre completo", width: 200 },
+  { field: "total", headerName: "Monto total", width: 100 },
+  {
+    field: "isPaid",
+    headerName: "Pagada",
+    width: 120,
+    align: "center",
+    renderCell: ({ row }: GridRenderCellParams) => {
+      return row.isPaid ? (
+        <Chip variant="outlined" label="Pagada" color="success" />
+      ) : (
+        <Chip variant="outlined" label="No pagada" color="error" />
+      );
+    },
+  },
+  {
+    field: "noProducts",
+    headerName: "No. Productos",
+    align: "center",
+    width: 120,
+  },
+  {
+    field: "createdAt",
+    headerName: "Creada en",
+    width: 200,
+  },
+  {
+    field: "check",
+    headerName: "Ver orden",
+    renderCell: ({ row }: GridRenderCellParams) => {
+      return (
+        <a href={`/admin/orders/${row.id}`} target="_blank" rel="noreferrer">
+          Ver orden
+        </a>
+      );
+    },
+  },
+];
+
+//* interfaces *//
+import { IOrder } from "../../interfaces/order";
+import { IUser } from "../../interfaces/user";
 
 const OrdersPage: NextPage = () => {
+  const { data, error } = useSWR<IOrder[]>("/api/admin/orders");
+
+  if (!data && !error) return <></>;
+
+  const rows = data!.map((order) => ({
+    id: order._id,
+    email: (order.user as IUser).email,
+    name: (order.user as IUser).name,
+    total: `$${order.total}`,
+    noProducts: order.numberOfItems,
+    isPaid: order.isPaid,
+    createdAt: order.createdAt,
+  }));
+
   return (
     <AdminLayout
       title="Ordenes"
       subtitle="Mantenimiento de ordenes"
       icon={<ConfirmationNumberOutlined />}
-    ></AdminLayout>
+    >
+      <Grid className="fadeIn" sx={{ mt: 2 }} container>
+        <Grid item xs={12} sx={{ height: 650, width: "100%" }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={10}
+            rowsPerPageOptions={[10]}
+          />
+        </Grid>
+      </Grid>
+    </AdminLayout>
   );
 };
 
