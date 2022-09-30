43fac4c3893880e3a062d8b2eee66e8a649ae07b
Tomas Cuevas <memo_mister398@hotmail.com>
2022-09-30 16:20:42 -0300

    Cambiar el rol del usuario

--- 05-teslo-shop/pages/admin/users.tsx
+++ 05-teslo-shop/pages/admin/users.tsx
@@ -1,6 +1,6 @@
 import type { GetServerSideProps, NextPage } from "next";
 import useSWR from "swr";
-import { Grid } from "@mui/material";
+import { Grid, MenuItem, Select } from "@mui/material";
 import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 import { PeopleOutline } from "@mui/icons-material";
 
@@ -10,6 +10,9 @@ import { AdminLayout } from "../../components/layouts/AdminLayout";
 //* utils *//
 import { verifyAdminInPage } from "../../utils/verifyAdminInPage";
 
+//* api *//
+import tesloApi from "../../api/tesloApi";
+
 //* interfaces *//
 import { IUser } from "../../interfaces/user";
 
@@ -18,10 +21,36 @@ const UsersPage: NextPage = () => {
 
   if (!data && !error) return <></>;
 
+  const onRoleUpdated = async (userId: string, newRole: string) => {
+    try {
+      await tesloApi.put("/admin/users", { userId, newRole });
+    } catch (error) {
+      console.log(error);
+      alert("No se pudo actualizar el role del usuario.");
+    }
+  };
+
   const columns: GridColDef[] = [
     { field: "email", headerName: "Email", width: 250 },
     { field: "name", headerName: "Nombre completo", width: 250 },
-    { field: "role", headerName: "Rol", width: 250 },
+    {
+      field: "role",
+      headerName: "Rol",
+      width: 250,
+      renderCell: ({ row }: GridRenderCellParams) => {
+        return (
+          <Select
+            value={row.role}
+            label="Rol"
+            sx={{ width: "300px" }}
+            onChange={({ target }) => onRoleUpdated(row.id, target.value)}
+          >
+            <MenuItem value="admin">Admin</MenuItem>
+            <MenuItem value="client">Client</MenuItem>
+          </Select>
+        );
+      },
+    },
   ];
 
   const rows = data!.map((user) => ({
